7ce8338de208af856bd710578d10031d8f61eafb
RikoshetGames <ilyalew@mail.ru>
2024-05-30 23:44:43 +0300

    Up permission

--- users/permissions.py
+++ users/permissions.py
@@ -2,19 +2,10 @@ from rest_framework.permissions import BasePermission
 
 
 class IsModerator(BasePermission):
-    message = "Вы не являетесь модератором"
+    message = "Вы не являетесь модератором или администратором."
 
     def has_permission(self, request, view):
-        return request.user.groups.filter(name='moderator').exists()
-
-
-class IsOwner(BasePermission):
-    message = "Вы не владелец данного объекта"
-
-    def has_object_permission(self, request, view, obj):
-        if request.user == obj.creation_user:
-            return request.method in ('LIST', 'GET', 'PUT', 'PATCH', 'DELETE')
-        return False
+        return request.user.groups.filter(name='moderator').exists() or request.user.is_superuser
 
 
 class IsSuperUser(BasePermission):
@@ -24,7 +15,16 @@ class IsSuperUser(BasePermission):
         return request.user.is_superuser
 
 
-class IsUserOwner(BasePermission):
+class IsCreator(BasePermission):
+    message = "Вы не владелец данного объекта"
+
+    def has_object_permission(self, request, view, obj):
+        if request.user == obj.create_user:
+            return request.method in ('GET', 'PUT', 'PATCH', 'DELETE')
+        return False
+
+
+class IsUser(BasePermission):
     """Права доступа для пользователя"""
     message = "Вы не являетесь владельцем этой учетной записи"
 
