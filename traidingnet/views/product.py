376ee4983832cebbdcdc73b3fa9ad226f4076c59
RikoshetGames <ilyalew@mail.ru>
2024-05-30 23:44:54 +0300

    Up product

--- traidingnet/views/product.py
+++ traidingnet/views/product.py
@@ -3,13 +3,12 @@ from rest_framework.viewsets import ModelViewSet
 
 from traidingnet.models import Product
 from traidingnet.serializers.product import ProductSerializers
-from users.permissions import IsModerator, IsCreator
+from users.permissions import IsModerator, IsCreator, IsSuperUser
 
 
 class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializers
     queryset = Product.objects.all()
-    permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
         new_obj = serializer.save()
@@ -17,10 +16,10 @@ class ProductViewSet(ModelViewSet):
         new_obj.save()
 
     def get_permissions(self):
-        if self.action == ['create', 'list']:
+        if self.action == ['create', 'list', 'retrieve']:
             self.permission_classes = [IsAuthenticated]
-        elif self.action in ['update', 'partial_update', 'retrieve']:
+        elif self.action in ['update', 'partial_update']:
             self.permission_classes = [IsAuthenticated, IsCreator | IsModerator]
         elif self.action == 'destroy':
-            self.permission_classes = [IsAuthenticated, IsCreator]
+            self.permission_classes = [IsAuthenticated, IsCreator | IsSuperUser]
         return [permission() for permission in self.permission_classes]
