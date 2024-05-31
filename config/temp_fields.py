514dd27cd5ab9ed0d35f098863a4a8de062290ea
RikoshetGames <ilyalew@mail.ru>
2024-05-31 22:57:05 +0300

    New migrations

--- config/temp_fields.py
+++
@@ -1,6 +0,0 @@
-sup_name = models.CharField(max_length=200, verbose_name='Название')
-supplier_type = models.IntegerField(choices=supplier_type_list, verbose_name='Уровень сети', default=0)
-
-
-company_name = models.CharField(max_length=200, verbose_name='Название')
-seller_type = models.IntegerField(choices=seller_type_list, verbose_name='Уровень сети', default=0)
